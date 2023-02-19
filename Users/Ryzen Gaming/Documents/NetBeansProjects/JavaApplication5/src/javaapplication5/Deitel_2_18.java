82c7006e5e2ed995693d90a68e11dfb533d67f82
tukigaming <axelgeronimo0@gmail.com>
2023-02-18 18:53:54 -0600

    uno

---
+++ Users/Ryzen Gaming/Documents/NetBeansProjects/JavaApplication5/src/javaapplication5/Deitel_2_18.java
@@ -0,0 +1,15 @@
+public class Deitel_2_18
+ {            
+   public static void main(String args[])
+ {            
+  System.out.println("*********     ***       *       *");
+  System.out.println("*       *   *     *    ***     * *");
+  System.out.println("*       *  *       *  *****   *   *");
+  System.out.println("*       *  *       *    *    *     *");
+  System.out.println("*       *  *       *    *   *       *");
+  System.out.println("*       *  *       *    *    *     * ");
+  System.out.println("*       *   *     *     *     *   *");
+  System.out.println("*********     ***       *       *");
+  }           
+
+ }          
