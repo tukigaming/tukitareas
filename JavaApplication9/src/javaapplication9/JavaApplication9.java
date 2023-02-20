d9705613423e45739eeea3b7866972e5ffc8ba51
axel suazo <114781582+tukigaming@users.noreply.github.com>
2023-02-19 23:41:58 -0600

    Update JavaApplication9.java

--- JavaApplication9/src/javaapplication9/JavaApplication9.java
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -1,21 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package javaapplication9;
+import java.util.Scanner;
 
-/**
- *
- * @author Ryzen Gaming
- */
-public class JavaApplication9 {
 
-   
-    
+ 
+public class Conductores {
+
+  
+    public void calcularKmLitro() {
      
-    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+      
+        double kilometros = 0;
+      
+        double litros = 0;
+        
+        double totalKilometros = 0;
+       
         
         
+        double totalLitros = 0;
+
+        System.out.println("Deme el número de kilómetros recorridos: ");
+        kilometros = entrada.nextDouble();
+
+       
+        
+        while(kilometros > 0) {
+            System.out.println("Deme el número de litros usados: ");
+            litros = entrada.nextDouble();
+
+            totalKilometros += kilometros;
+            totalLitros += litros;
+
+            System.out.println("Kilometros-Litros para este "
+                    + "reabastecimiento: " + kilometros / litros);
+
+            System.out.println("Kilometros-Litros para todo "
+                    + "el recorrido: " + totalKilometros / totalLitros);
+
+            System.out.println(
+                    "Deme el número de kilómetros recorridos: ");
+            kilometros = entrada.nextDouble();
+        }
     }
-    
+
+
+    public static void main(String[] args) {
+        Conductores conductor = new Conductores();
+        conductor.calcularKmLitro();
+    }
 }
